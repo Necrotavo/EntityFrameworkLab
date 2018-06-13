a38ea05223f863f84916d53a5b756f3bc9864ca8
Necrotavo <gapalomo.u@gmail.com>
2018-06-12 21:56:41 -0600

    productInsert ya valida

--- EntityFrameworkLab/productInsert.aspx.cs
+++ EntityFrameworkLab/productInsert.aspx.cs
@@ -13,22 +13,41 @@ namespace EntityFrameworkLab
         BL_Producto blProduct = new BL_Producto();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            lblError.Text = "";
         }
 
         protected void btnExecute_Click(object sender, EventArgs e)
         {
-            blProduct.Codigo = TextBox1.Text;
-            blProduct.Descripcion = TextBox2.Text;
-            blProduct.Precio = Convert.ToInt16(TextBox3.Text);
-            blProduct.Cantidad_Inventario = Convert.ToInt16(TextBox4.Text);
+            try
+            {
+                blProduct.Codigo = TextBox1.Text;
+                blProduct.Descripcion = TextBox2.Text;
+                blProduct.Precio = Convert.ToInt16(TextBox3.Text);
+                blProduct.Cantidad_Inventario = Convert.ToInt16(TextBox4.Text);
 
-            blProduct.insertProduct();
+                blProduct.insertProduct();
+                clearTxtbx();
+                lblError.Text = "El producto se ha ingresado correctamente";
+            }
+            catch (Exception)
+            {
+
+                clearTxtbx();
+                lblError.Text = "Error al ingresar producto";
+            }
+
         }
 
         protected void btnSalir_Click(object sender, EventArgs e)
         {
             Response.Redirect("MenuProductos.aspx");
+        }
+        protected void clearTxtbx()
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
         }
     }
 }
--- EntityFrameworkLab/productInsert.aspx.designer.cs
+++ EntityFrameworkLab/productInsert.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace EntityFrameworkLab {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// lblError control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
+        
+        /// <summary>
         /// lblCod control.
         /// </summary>
         /// <remarks>
@@ -38,6 +47,15 @@ namespace EntityFrameworkLab {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
         
         /// <summary>
         /// lblDesc control.
@@ -58,6 +76,15 @@ namespace EntityFrameworkLab {
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
         
         /// <summary>
+        /// RequiredFieldValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+        
+        /// <summary>
         /// lblPrice control.
         /// </summary>
         /// <remarks>
@@ -76,6 +103,15 @@ namespace EntityFrameworkLab {
         protected global::System.Web.UI.WebControls.TextBox TextBox3;
         
         /// <summary>
+        /// RequiredFieldValidator3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+        
+        /// <summary>
         /// lblCantidad control.
         /// </summary>
         /// <remarks>
@@ -92,6 +128,15 @@ namespace EntityFrameworkLab {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox4;
+        
+        /// <summary>
+        /// RequiredFieldValidator4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator4;
         
         /// <summary>
         /// btnExecute control.
